90505b386365071ff8f4804c9e4ec14b072d7952
oalvluc0702 <oskitar760@gmail.com>
2025-10-06 10:36:59 +0200

    añadido comentarios al ejercicio1

--- practica2/src/ejercicio1.java
+++ practica2/src/ejercicio1.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 public class ejercicio1 {
     public static void main(String[] args){
+        //declaramos scanner y creamos un array con longitud 10
         Scanner s = new Scanner(System.in);
         int[] numeros = new int[10];
+        //un bucle que realiza iteraciones hasta que sea menor que la longitud del array y mete los números dentro del arra
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("Introduce un valor");
             numeros[i] = s.nextInt();
         }
+        // muestra los números por pantalla junto al índice
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("El índice es: "+i+" y el número introducido es: "+numeros[i]);
         }
