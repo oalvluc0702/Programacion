9e796de9227fbcc157a06f384085393a985d8da4
oalvluc0702 <oskitar760@gmail.com>
2025-10-06 11:41:07 +0200

    ejercicio2 realizado

---
+++ practica2/src/ejercicio2.java
@@ -0,0 +1,11 @@
+public class ejercicio2 {
+    public static void main(String[] args){
+        //definimos un array con diferentes valores
+        int[] numeros = {1,2,3,4,5,6,7,8};
+
+        //lo recorremos con un for concatenando el numero con 1 espacio en blanco
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i]+" ");
+        }
+    }
+}
