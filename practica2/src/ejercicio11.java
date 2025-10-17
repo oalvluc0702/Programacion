e27a2d178259845c2452d07941b269d9c564ccb9
oalvluc0702 <oskitar760@gmail.com>
2025-10-17 10:49:20 +0200

    Cambio en el ejercicio 11 para que funcione como se espera

--- practica2/src/ejercicio11.java
+++ practica2/src/ejercicio11.java
@@ -2,21 +2,18 @@ import java.util.Arrays;
 
 public class ejercicio11 {
     public static void main(String[] args){
-        //definimos el array nums y definimos una variable contador y 2 variables que servirán para controlar los numeros
+        //definimos el array nums y un array auxiliar para invertirlo
         int[] nums={1,2,3,4,5,6};
-        int contador=0;
-        int valoractual;
-        int valorfinal;
-
-        //ahora con un for recorremos el array de manera inversa y guardamos como valor actual lo que se supone que vale la primera posición y luego la última con la variable del bucle
+        int[] numAux= new int[6];
+        int contador = 0;
+        //ahora con un for recorremos el array de manera inversa y guardamos los datos invertidos en el array auxiliar
         for (int i = nums.length; i > 0; i--) {
-            valoractual=nums[contador];
-            valorfinal=nums[i-1];
-            //ahora sobreescribimos el array invirtiendo los valores del principio y de la cola
-            nums[contador]=valoractual;
-            nums[i-1]=valorfinal;
-            //por cada iteración usaremos este contador para contar por el principio del array
+            numAux[contador]=nums[i-1];
             contador++;
+        }
+        //sobreescribimos el array nums con el contenido del auxiliar
+        for (int k = 0; k < numAux.length; k++) {
+            nums[k]=numAux[k];
         }
         //ahora con el metodo Arrays.toString() mostramos por pantalla el array como si fuera un texto para comprobar que se ha invertido.
         System.out.println(Arrays.toString(nums));
