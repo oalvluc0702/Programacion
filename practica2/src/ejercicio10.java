4246bc2c8825986cbda6f6e69096d56907431914
oalvluc0702 <oskitar760@gmail.com>
2025-10-17 10:04:08 +0200

    ejercicio10 y ejercicio10 bidimensional hechos nuevamente

--- practica2/src/ejercicio10.java
+++ practica2/src/ejercicio10.java
@@ -2,19 +2,31 @@ import java.util.Scanner;
 
 public class ejercicio10 {
     public static void main(String[] args){
-        //definimos la clase Scanner y los array de estudiantes y notass
+        //definimos la clase Scanner y los array de estudiantes y notas
         Scanner s = new Scanner(System.in);
-        String[] estudiantes={"Óscar","Mario","Adrián","Paconi","Gorka"," "};
-        double[] notas={10,5,7,9,6.5,0};
+        String[] estudiantes={"Óscar","Mario","Adrián","Paconi","Gorka"};
+        double[] notas={10,5,7,9,6.5,};
+        //Defino la variable de estudiante solicitado y cual es la posición del último estudiante
+        String estudianteSol;
+        int ultimoEst= estudiantes.length - 1;
 
-        //Pedimos por pantalla el nombre del alumno y la nota en la posición placeholder.
+        //Pedimos por teclado el nombre del alumno
         System.out.println("Dime tu nombre de alumno");
-        estudiantes[5] = s.nextLine();
-        System.out.println("Dime tu nota en programación");
-        notas[5] = s.nextDouble();
+        estudianteSol = s.nextLine();
 
-        //Imprimimos por pantalla la nota del alumno y su nombre
+        //Con un bucle vamos recorriendo el bucle de alumnos comprobando si es igual al nombre que nos ha solicitado
+        for (int i = 0; i < estudiantes.length; i++) {
 
-        System.out.printf("La nota del alumno %s es: %.2f",estudiantes[5],notas[5]);
+            //si lo encuentra muestra el alumno y la nota
+            if (estudianteSol.equals(estudiantes[i])) {
+                System.out.printf("La nota del alumno %s es: %.2f", estudiantes[i], notas[i]);
+                break;
+            }
+            //si es la última posición y el estudiante no coincide con el campo, va a decirnos que no existe el alumno que ha solicitado
+            else if (i == ultimoEst && !estudianteSol.equals(estudiantes[i])) {
+                System.out.println("No existe ese alumno solicitado");
+                break;
+            }
+        }
     }
 }
--- practica2/src/ejercicio10Bi.java
+++ practica2/src/ejercicio10Bi.java
@@ -2,18 +2,25 @@ import java.util.Scanner;
 
 public class ejercicio10Bi {
     public static void main(String[] args){
-        //Definimos la clase Scanner y el array bidimensional con el que vamos a guardar los datos
+        //Definimos la clase Scanner y el array bidimensional con el que residen los datos
         Scanner s = new Scanner(System.in);
-        String[][] estudianteNota= {{"Óscar","10"},{"Gorka","10"},{"Mario","7"},{"",""}};
+        String[][] estudianteNota= {{"Óscar","10"},{"Gorka","10"},{"Mario","7"}};
 
-        //Ahora pedimos los datos y los almacenamos en el array bidimensional.
+        //guardamos el estudiante que pide por pantalla en una variable
         System.out.println("dime un el nombre del alumno");
-        estudianteNota[3][0] = s.nextLine();
-
-        System.out.println("Dime la nota de programación");
-        estudianteNota[3][1]  = s.nextLine();
-
-        //ahora imprimimos por pantalla el alumno más su nota.
-        System.out.printf("El alumno %s tiene nota: %s",estudianteNota[3][0],estudianteNota[3][1]);
+        String estudianteSol = s.nextLine();
+        //guardamos la ultima posicion del array en una variable
+        int ultimoEst = estudianteNota.length -1;
+        //ahora recorremos el array bidimensional comparando si los los nombres son iguales que la entrada
+        for (int i = 0; i < estudianteNota.length; i++) {
+            if (estudianteSol.equals(estudianteNota[i][0])){
+                System.out.printf("La nota del alumno %s es: %s",estudianteNota[i][0],estudianteNota[i][1]);
+                break;
+            }
+            //si no lo encuentra en la ultima iteración te muestra por pantalla un texto explicativo
+            else if (i == ultimoEst && !estudianteSol.equals(estudianteNota[i][0])) {
+                System.out.println("No existe el alumno solicitado");
+            }
+        }
     }
 }
