9408a99ee00fe983f8035a0ddcebabf18a213617
oalvluc0702 <oskitar760@gmail.com>
2025-10-27 13:11:03 +0100

    ejercicio1 hecho correctamente y comentado

--- practica3/src/ejercicio1.java
+++ practica3/src/ejercicio1.java
@@ -1,3 +1,7 @@
+/*Define un array de números tipo double de 3 filas por 7 columnas con nombre doub y
+asigna los valores según la siguiente tabla. Muestra el contenido de todos los elementos del
+array dispuestos en forma de tabla como se muestra en la figura. */
+
 public class ejercicio1 {
     public static void main(String[] args){
         //creamos el array bidimensional de numeros usando 9999 como plantilla de casilla en blanco
