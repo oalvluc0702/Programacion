0ded5244ec46247bb410985c3e9209be88957221
oalvluc0702 <oskitar760@gmail.com>
2025-11-14 10:55:23 +0100

    Practica de Opcional terminada

--- practica2/src/ejercicio6.java
+++ practica2/src/ejercicio6.java
@@ -12,8 +12,9 @@ public class ejercicio6 {
         int[] nums = new int[10];
 
         //ahora definimos las variables para contar los números
-        int pares = 0;
-        int impares = 0;
+        int positivo = 0;
+        int negativo = 0;
+        int ceros = 0;
 
         //metemos los números en el array
         for (int i = 0; i < nums.length; i++) {
@@ -22,14 +23,16 @@ public class ejercicio6 {
         }
         // ahora hacemos una comprobación de los números y vamos contando dependiendo de si son mayores menor o igual a 0
         for (int k = 0; k < nums.length; k++) {
-            if (nums[k] % 2 == 0){
-                pares++;
-            } else {
-                impares++;
+            if (nums[k] > 0){
+                positivo++;
+            } else if (nums[k] < 0){
+                negativo++;
+            } else if (nums[k] == 0){
+                ceros++;
             }
         }
-        System.out.println("Hay "+pares+" números positivos");
-        System.out.println("Hay "+impares+" números negativos");
-        //System.out.println("Hay "+ceros+" ceros");
+        System.out.println("Hay "+positivo+" números positivos");
+        System.out.println("Hay "+negativo+" números negativos");
+        System.out.println("Hay "+ceros+" ceros");
     }
 }
