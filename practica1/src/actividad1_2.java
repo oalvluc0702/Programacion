c0f1c2f287fb4aa5d89c619c0041a1a34613ade7
phantomdancer76 <oskitar760@gmail.com>
2025-12-19 14:09:41 +0100

    eliminado cosas de practicas anteriores y añadidos los paquetes

--- practica1/src/actividad1_2.java
+++
@@ -1,9 +0,0 @@
-public class actividad1_2 {
-    public static void main(String[] args){
-        String nombre = "Óscar";
-        String direcc = "Calle Panamá, 8, 2-I";
-        int telef = 671332546;
-        System.out.println("Me llamo "+nombre+" mi dirección es: "+direcc+" mi teléfono es "+telef);
-
-    }
-}
--- practica1/src/actividad2_4.java
+++
@@ -1,8 +0,0 @@
-public class actividad2_4 {
-    public static void main(String[] args){
-        for (int i = 0; i <= 100; i++) {
-            int resultado = 5*i;
-            System.out.println(resultado);
-        }
-    }
-}
