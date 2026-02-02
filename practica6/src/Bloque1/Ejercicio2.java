65f3489476e1583673484587a818349a03f28aa6
oalvluc0702 <oskitar760@gmail.com>
2026-02-02 14:12:19 +0100

    Ejercicio2 completado

---
+++ practica6/src/Bloque1/Ejercicio2.java
@@ -0,0 +1,14 @@
+package Bloque1;
+
+import java.util.HashSet;
+
+public class Ejercicio2 {
+    public static void main(String[] args){
+        HashSet<String> censoVillanos = new HashSet<>();
+        censoVillanos.add("Morgott");
+        censoVillanos.add("Morgott");
+        censoVillanos.add("Barba negra");
+        censoVillanos.add("Pedro sanchez");
+        System.out.println(censoVillanos.size());
+    }
+}
