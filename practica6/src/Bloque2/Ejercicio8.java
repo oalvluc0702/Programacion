2db8dcaf4bdc92fb728e1d80a3d54d096397259f
oalvluc0702 <oskitar760@gmail.com>
2026-02-06 09:09:47 +0100

    Ejercicio8 completado correctamente

--- practica6/src/Bloque2/Ejercicio8.java
+++ practica6/src/Bloque2/Ejercicio8.java
@@ -2,26 +2,25 @@ package Bloque2;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Ejercicio8 {
-    public static void mostrarGremio(HashMap<String, ArrayList<String>> repositorio, String gremio){
-        for(String miembro: repositorio.get(gremio)){
-            System.out.println(miembro);
-        }
-    }
     public static void main(String[] args){
-
-        HashMap<String, ArrayList<String>> repositorioGremial = new HashMap<>();
-        ArrayList<String> magos = new ArrayList<>();
-        ArrayList<String> guerreros = new ArrayList<>();
-        magos.add("Oscar");
-        magos.add("Mario");
-        magos.add("Gandalf");
-        guerreros.add("Percival");
-        guerreros.add("Vane");
-        guerreros.add("Narmaya");
-        repositorioGremial.put("magos",magos);
-        repositorioGremial.put("guerreros",guerreros);
-        mostrarGremio(repositorioGremial, "magos");
+           HashMap<String, HashSet<String>> monstruoLoot = new HashMap<>();
+           HashSet<String> trasgo = new HashSet<>();
+           trasgo.add("Espada Oxidada");
+           trasgo.add("Ropaje Viejo");
+           trasgo.add("Oreja Duende");
+           trasgo.add("Espada Oxidada");
+           HashSet<String> dragon = new HashSet<>();
+           dragon.add("Escama de Dragón");
+           dragon.add("Ojo de Dragón");
+           dragon.add("Lanza del Matadragones");
+           monstruoLoot.put("trasgo", trasgo);
+           monstruoLoot.put("dragon", dragon);
+           monstruoLoot.get("dragon").add("Lanza del Matadragones");
+           for (String monstruo: monstruoLoot.keySet()){
+               System.out.println(monstruo+ ": "+ monstruoLoot.get(monstruo));
+           }
     }
 }
