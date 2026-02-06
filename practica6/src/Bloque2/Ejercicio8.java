206c505cd5af04d8b1ebe02d527ef608d28dfde0
oalvluc0702 <oskitar760@gmail.com>
2026-02-06 08:42:30 +0100

    Ejercicio8 completado

---
+++ practica6/src/Bloque2/Ejercicio8.java
@@ -0,0 +1,27 @@
+package Bloque2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Ejercicio8 {
+    public static void mostrarGremio(HashMap<String, ArrayList<String>> repositorio, String gremio){
+        for(String miembro: repositorio.get(gremio)){
+            System.out.println(miembro);
+        }
+    }
+    public static void main(String[] args){
+
+        HashMap<String, ArrayList<String>> repositorioGremial = new HashMap<>();
+        ArrayList<String> magos = new ArrayList<>();
+        ArrayList<String> guerreros = new ArrayList<>();
+        magos.add("Oscar");
+        magos.add("Mario");
+        magos.add("Gandalf");
+        guerreros.add("Percival");
+        guerreros.add("Vane");
+        guerreros.add("Narmaya");
+        repositorioGremial.put("magos",magos);
+        repositorioGremial.put("guerreros",guerreros);
+        mostrarGremio(repositorioGremial, "magos");
+    }
+}
