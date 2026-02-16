09139b0e11ecaf8151b7222e61e63e1c8940cd26
oalvluc0702 <oskitar760@gmail.com>
2026-02-16 12:15:03 +0100

    Comida hecho

---
+++ entornosPractica/src/Comida.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Comida extends Producto{
+    private HashSet<String> alergenos;
+    private int tiempoCocinado;
+
+    public Comida(String nombre, ArrayList<Producto> comboFiesta, double precio, int descuento, HashSet<String> alergenos, int tiempoCocinado) {
+        super(nombre, comboFiesta, precio, descuento);
+        this.alergenos = alergenos;
+        this.tiempoCocinado = tiempoCocinado;
+    }
+
+    public HashSet<String> getAlergenos() {
+        return alergenos;
+    }
+
+    public int getTiempoCocinado() {
+        return tiempoCocinado;
+    }
+}
