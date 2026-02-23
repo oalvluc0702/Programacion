0bb0893d29b2a6ff7ff508d6cfe3b40be3643832
oalvluc0702 <oskitar760@gmail.com>
2026-02-23 14:09:55 +0100

    JSON creado correctamente

--- practica7/src/com/rpg/utils/JsonHelper.java
+++ practica7/src/com/rpg/utils/JsonHelper.java
@@ -1,8 +1,13 @@
 package com.rpg.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class JsonHelper {
@@ -11,9 +16,12 @@ public class JsonHelper {
 
     public <T> List<T> readlist(String path, Class<T> tClass){
         try (Reader reader = new FileReader(path)){
-
+            Type typetoken = TypeToken.getParameterized(List.class, tClass).getType();
+            Gson gson = new Gson();
+            return  gson.fromJson(reader, typetoken);
         } catch (IOException e){
             System.out.println("no se ha encontrado el archivo");
+            return List.of();
         }
     }
 }
