0abb08679aa1e54ed1ab7e8813dfb58fa5331cec
oalvluc0702 <oskitar760@gmail.com>
2026-02-25 10:44:20 +0100

    Excepciones creadas

--- practica7/src/com/rpg/main/Main.java
+++ practica7/src/com/rpg/main/Main.java
@@ -1,22 +1,9 @@
 package com.rpg.main;
 
-import com.rpg.model.Ciudades;
-import com.rpg.model.Personajes;
-import com.rpg.utils.JsonHelper;
-import com.rpg.utils.TxtHelper;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import com.rpg.handler.RPGDataException;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
-        TxtHelper txtHelper = new TxtHelper();
-        txtHelper.cargarFichero("practica7/ficheros/ciudades.txt");
-        JsonHelper jsonHelper = new JsonHelper();
-        List<Personajes> personajes = jsonHelper.readlist("practica7/ficheros/personajes.json", Personajes.class);
-        for (Personajes personaje: personajes){
-            System.out.println(personaje.getNombre()+": "+personaje.getNivel());
-        }
+    public static void main(String[] args) throws RPGDataException {
+
     }
 }
